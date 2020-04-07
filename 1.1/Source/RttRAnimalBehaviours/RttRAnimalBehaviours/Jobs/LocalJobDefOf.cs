c5b6d06140c513a1175d393efe59e89e6309c448
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-04-07 21:05:48 +0200

    v0.202

---
+++ 1.1/Source/RttRAnimalBehaviours/RttRAnimalBehaviours/Jobs/LocalJobDefOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Verse;
+using RimWorld;
+
+namespace RttRAnimalBehaviours
+{
+    [DefOf]
+    public static class LocalJobDefOf
+    {
+        public static JobDef RttR_AnimalResource;
+    
+
+    }
+
+}
---
+++ 1.1/Source/RttRAnimalBehaviours/RttRAnimalBehaviours/Pawn_Spiky.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace RttRAnimalBehaviours
+{
+    public class Pawn_Spiky: Pawn
+    {
+        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.PostApplyDamage(dinfo, totalDamageDealt);
+            Pawn instigator = dinfo.Instigator as Pawn;
+            if (instigator != null && instigator.Map!=null)
+            {
+                CellRect rect = GenAdj.OccupiedRect(instigator.Position, instigator.Rotation, IntVec2.One).ExpandedBy(1);
+                foreach (IntVec3 current in rect.Cells)
+                {
+                    if (current.InBounds(instigator.Map))
+                    {
+                        HashSet<Thing> hashSet = new HashSet<Thing>(current.GetThingList(this.Map));
+                        if (hashSet != null)
+                        {
+                            foreach (Thing thing in hashSet)
+                            {
+                                if (thing == this)
+                                {
+                                    instigator.TakeDamage(new DamageInfo(DamageDefOf.Cut, 20, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown));
+
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+           
+        }
+
+    }
+}
