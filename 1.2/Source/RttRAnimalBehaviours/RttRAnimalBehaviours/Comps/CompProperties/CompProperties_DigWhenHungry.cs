35b214692b223eca02b16472e6491802a44da2d0
sargbjornson <sarg.auxiliar@gmail.com>
2020-08-13 22:09:16 +0100

    v0.3

---
+++ 1.2/Source/RttRAnimalBehaviours/RttRAnimalBehaviours/Comps/CompProperties/CompProperties_DigWhenHungry.cs
@@ -0,0 +1,23 @@
+﻿
+using Verse;
+
+namespace RttRAnimalBehaviours
+{
+    public class CompProperties_DigWhenHungry : CompProperties
+    {
+
+
+        public string customThingToDig = "";
+        public int customAmountToDig = 1;
+        public int timeToDig = 40000;
+
+
+
+        public CompProperties_DigWhenHungry()
+        {
+            this.compClass = typeof(CompDigWhenHungry);
+        }
+
+
+    }
+}
